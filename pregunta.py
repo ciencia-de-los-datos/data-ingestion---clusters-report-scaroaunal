371965ba7be44821d3c0d5c0987c491567112f2b
scaroaunal <scaroa@unal.edu.co>
2023-04-09 21:32:58 -0500

    scaroa solución LAB4

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,22 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    nombres= ["cluster","cantidad_de_palabras_clave","porcentaje_de_palabras_clave","principales_palabras_clave"]
+    df = pd.read_fwf("clusters_report.txt",names = nombres,skiprows=4)
 
+    df["cluster"].fillna(method="ffill",inplace=True)
+    df["cantidad_de_palabras_clave"].fillna(method="ffill",inplace=True)
+    df["porcentaje_de_palabras_clave"].fillna(method="ffill",inplace=True)
+    df = df.groupby(["cluster","cantidad_de_palabras_clave","porcentaje_de_palabras_clave"])["principales_palabras_clave"].apply(" ".join).reset_index()    
+    df["porcentaje_de_palabras_clave"].replace(" %","", regex=True, inplace=True)
+    df["porcentaje_de_palabras_clave"].replace(",",".", regex=True, inplace=True)
+    df["porcentaje_de_palabras_clave"]=df["porcentaje_de_palabras_clave"].astype(float)
+    df["principales_palabras_clave"]=df["principales_palabras_clave"].map(lambda x:x.replace(".",""))
+    df['principales_palabras_clave']=df['principales_palabras_clave'].replace('\s+', ' ', regex=True)
+    #df["porcentaje_de_palabras_clave"].replace("","%",inplace=True)    
     return df
+
+
+
+
+
